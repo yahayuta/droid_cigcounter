337f640020f769b6c015c2c0eec58784fbd09984
yahayuta <yasunariishizu@hotmail.com>
2021-02-15 09:05:31 +0900

    version up for 3.0.0

--- app/build/generated/source/buildConfig/debug/droid/cigcounter/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/droid/cigcounter/BuildConfig.java
@@ -7,7 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "droid.cigcounter";
   public static final String BUILD_TYPE = "debug";
-  public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 44;
-  public static final String VERSION_NAME = "2.8.0";
+  public static final int VERSION_CODE = 45;
+  public static final String VERSION_NAME = "3.0.0";
 }
