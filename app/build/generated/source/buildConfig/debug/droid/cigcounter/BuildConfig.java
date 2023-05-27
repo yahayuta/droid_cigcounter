094966d28c3b52f2dee176640109c35d6f8a5d32
やはゆた <13937582+yahayuta@users.noreply.github.com>
2023-05-27 12:08:40 +0900

    change permissions

--- app/build/generated/source/buildConfig/debug/droid/cigcounter/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/droid/cigcounter/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "droid.cigcounter";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 45;
-  public static final String VERSION_NAME = "3.0.0";
+  public static final int VERSION_CODE = 46;
+  public static final String VERSION_NAME = "3.1.0";
 }
